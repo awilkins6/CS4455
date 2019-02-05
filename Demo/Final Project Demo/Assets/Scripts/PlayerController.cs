762d02290e5e37503681d574ee3844fe3fd0eba9
Alexander Wilkins <awilkins6@gatech.edu>
2019-02-04 22:05:45 -0500

    demo

---
+++ Demo/Final Project Demo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody rb;
+
+	public float speed;
+
+	public float jumpPower;
+
+	public GameObject bullet;
+	public Transform bulletSpawn;
+
+	public float fireRate;
+	private float nextFire;
+
+    void Start() {
+    	rb = GetComponent<Rigidbody>();
+    }
+
+    void update() {
+    	if (Input.GetButton("Fire1") && Time.time > nextFire) {
+    		nextFire = Time.time + fireRate;
+    		Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+    	}
+    }
+
+    void FixedUpdate() {
+    	float moveHorizontal = Input.GetAxis("Horizontal");
+    	float moveVertical = Input.GetAxis("Vertical");
+
+    	Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+    	rb.AddForce(movement * speed);
+
+    	updateJump();
+    }
+
+    // void updateShoot() {
+    // 	Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+    // }
+
+  //   void updateHook() {
+  //   	if (Input.GetKeyDown("z")) {
+			
+		// }
+  //   }
+
+    void updateJump() {
+    	if (Input.GetKeyDown("space")) {
+			Vector3 movement = new Vector3(0.0f, 1.0f, 0.0f);
+			rb.AddForce(movement * jumpPower);
+		}
+    }
+}
