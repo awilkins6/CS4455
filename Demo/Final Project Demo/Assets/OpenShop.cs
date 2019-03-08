e2a646d84f3fad8f48fd5be31a113c7cd2142f86
Eugene Ahn <eahn2016@gmail.com>
2019-03-08 14:58:16 -0500

    Opens up shop UI when near spaceman
    Press 'E' when near the spaceman to open up the shop

--- Demo/Final Project Demo/Assets/OpenShop.cs
+++ Demo/Final Project Demo/Assets/OpenShop.cs
@@ -35,23 +35,23 @@ public class OpenShop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyUp(KeyCode.E))
-        //{
-        //    if (shop.interactable)
-        //    {
-        //        shop.interactable = false;
-        //        shop.blocksRaycasts = false;
-        //        shop.alpha = 0f;
-        //        Time.timeScale = 1f;
-        //    }
-        //    else
-        //    {
-        //        shop.interactable = true;
-        //        shop.blocksRaycasts = true;
-        //        shop.alpha = 1f;
-        //        Time.timeScale = 0f;
-        //    }
-        //}
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            if (shop.interactable)
+            {
+                shop.interactable = false;
+                shop.blocksRaycasts = false;
+                shop.alpha = 0f;
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                shop.interactable = true;
+                shop.blocksRaycasts = true;
+                shop.alpha = 1f;
+                Time.timeScale = 0f;
+            }
+        }
       
     }
 }
