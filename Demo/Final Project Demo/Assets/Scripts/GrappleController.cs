52c42139a7c35085d2d60c5ff3ff044b78a47a9a
Cesar <cesarporcayo@gatech.edu>
2019-03-09 14:11:16 -0500

    started grappling functionaility and added to lake

---
+++ Demo/Final Project Demo/Assets/Scripts/GrappleController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleController : MonoBehaviour
+{
+    public GameObject hook;
+    public GameObject hookHolder;
+
+    public float hookTravelSpeed;
+    public float playerTravelSpeed;
+
+    public bool hooked;
+    public static bool fired;
+
+    public float maxDistance;
+    private float currentDistance;
+
+    void Update()
+    {
+        //firing hook
+        if (Input.GetMouseButton(0) && fired == false)
+        {
+            fired = true;
+
+        }
+
+        if (fired == true && hooked == false)
+        {
+            hook.transform.Translate(Vector3.forward * Time.deltaTime * hookTravelSpeed);
+            currentDistance = Vector3.Distance(transform.position, hook.transform.position);
+
+            if (currentDistance >= maxDistance)
+            {
+                ReturnHook();
+            }
+        }
+
+        if (hooked == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, hook.transform.position, Time.deltaTime * playerTravelSpeed);
+            float distanceToHook = Vector3.Distance(transform.position, hook.transform.position);
+
+            this.GetComponent<Rigidbody>().useGravity = false;
+
+            if (distanceToHook < 1)
+            {
+                ReturnHook();
+            }
+
+        } else
+        {
+            this.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    void ReturnHook()
+    {
+        hook.transform.position = hookHolder.transform.position;
+        fired = false;
+        hooked = false;
+    }
+}
