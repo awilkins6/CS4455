f569a70e30ea8fb581974eba9c3f3a84f13716a6
admin <admin@admins-MBP.hsd1.ga.comcast.net>
2019-03-10 21:55:56 -0400

    enemies initial

---
+++ Demo/Final Project Demo/Assets/Scripts/TurretScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour
+{
+
+    public enum AIState
+    {
+        Idle,
+        Targeting
+    };
+
+    public AIState aiState;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aiState = AIState.Idle;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+}
