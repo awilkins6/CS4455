e131a17cdd072f4387f337aee8992ac73fb2881d
admin <admin@admins-MBP.hsd1.ga.comcast.net>
2019-03-10 22:36:24 -0400

    Merge branch 'level-hook-time-merge'

---
+++ Demo/Final Project Demo/Assets/AttachHook.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachHook : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 0.3f, target.transform.position.z - 0.6f);
+    }
+
+}
