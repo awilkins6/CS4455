52285ce88856afe1b36642e4c1e555b5e59af082
admin <admin@admins-MBP.hsd1.ga.comcast.net>
2019-03-10 23:45:46 -0400

    final merge for built submission

---
+++ Demo/Final Project Demo/Assets/Scripts/TurretScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour
+{
+
+    public enum AIState
+    {
+        Idle,
+        Targeting
+    };
+
+    public AIState aiState;
+    public GameObject target;
+
+    public GameObject head;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aiState = AIState.Idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+      if (other.GetComponent<AlienScript>() != null) {
+        head.transform.LookAt(other.transform.position);
+      }
+    }
+}
